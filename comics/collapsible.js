dddbb24346efa31de26c9c7819090c50152248a4
Tyler Morgan <107895555+TylerJMorg@users.noreply.github.com>
2024-08-12 19:06:27 -0600

    Add new comics to inv.

--- comics/collapsible.js
+++ comics/collapsible.js
@@ -1,54 +1,57 @@
 function initializeCollapsible() {
-    let coll = document.getElementsByClassName("collapsible");
-  
-    for (let i = 0; i < coll.length; i++) {
-      coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-  
-        // Get the next sibling element
-        let content = document.getElementById(this.getAttribute("data-target-id"));
-  
-        // Debugging: Check if content is found
-        console.log("Clicked element:", this);
-        console.log("Next sibling element:", content);
-  
-        // Handle case when content is null
-        if (content && content.classList.contains("content-1")) {
-          if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-          } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-          }
+  let coll = document.getElementsByClassName("collapsible");
+
+  for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+
+      // Get the target content element
+      let content = document.getElementById(this.getAttribute("data-target-id"));
+      let parentComic = this.closest('.comic');
+
+      // Debugging: Check if content is found
+      console.log("Clicked element:", this);
+      console.log("Next sibling element:", content);
+
+      // Handle case when content is null
+      if (content && content.classList.contains("content-1")) {
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+          parentComic.style.maxHeight = null;
         } else {
-          console.error("No content element found for:", this);
-          // Optional: Provide feedback to the user or take alternative actions
+          content.style.maxHeight = content.scrollHeight + "px";
+          parentComic.style.maxHeight = parentComic.scrollHeight + content.scrollHeight + "px";
         }
-      });
-    }
-  
-    // Accessible for screen readers and tab indexing.
-    document.querySelectorAll("[id^='aboutMeButton-']").forEach(button => {
-      button.addEventListener("click", function() {
-        let targetId = this.getAttribute("data-target-id");
-        let element = document.getElementById(targetId);
-        let contentLinks = element ? element.querySelectorAll(".content-link, .dark-btn, a") : [];
-  
-        if (this.getAttribute("aria-expanded") === "true") {
-          this.setAttribute("aria-expanded", "false");
-          if (element) {
-            element.setAttribute("aria-hidden", "true");
-            contentLinks.forEach(link => link.setAttribute("tabindex", "-1"));
-          }
-        } else {
-          this.setAttribute("aria-expanded", "true");
-          if (element) {
-            element.setAttribute("aria-hidden", "false");
-            contentLinks.forEach(link => link.setAttribute("tabindex", "0"));
-          }
-        }
-      });
+      } else {
+        console.error("No content element found for:", this);
+        // Optional: Provide feedback to the user or take alternative actions
+      }
     });
-  
-    console.log('Collapsible initialized');
   }
+
+  // Accessible for screen readers and tab indexing.
+  document.querySelectorAll("[id^='aboutMeButton-']").forEach(button => {
+    button.addEventListener("click", function() {
+      let targetId = this.getAttribute("data-target-id");
+      let element = document.getElementById(targetId);
+      let contentLinks = element ? element.querySelectorAll(".content-link, .dark-btn, a") : [];
+
+      if (this.getAttribute("aria-expanded") === "true") {
+        this.setAttribute("aria-expanded", "false");
+        if (element) {
+          element.setAttribute("aria-hidden", "true");
+          contentLinks.forEach(link => link.setAttribute("tabindex", "-1"));
+        }
+      } else {
+        this.setAttribute("aria-expanded", "true");
+        if (element) {
+          element.setAttribute("aria-hidden", "false");
+          contentLinks.forEach(link => link.setAttribute("tabindex", "0"));
+        }
+      }
+    });
+  });
+
+  console.log('Collapsible initialized');
+}
   
--- comics/comicgrid.js
+++ comics/comicgrid.js
@@ -1,59 +1,60 @@
 function initializeComicGrid() {
- const columnOrder = ["idFile", "title", "qty", "publisher", "year", "writers", "illustrators", "era", "cgc", "idLink", "notes"];
- 
- fetch('/comics/comics.json')
-   .then(response => response.json())
-   .then(data => {
-     let table = '<ul class="comic-grid">';
-     data.forEach((item, index) => {
-       table += '<li class="comic">';
-       columnOrder.forEach(column => {
-         if (column === 'idFile') {
-           table += `<div><img class="comic-img" alt="Comic cover" src="/comics/img/covers/${item.id}.avif">`;
-         } else if (column === 'title') {
-           const issue = item.issue ? ` #${item.issue}` : '';
-           const lgy = item.lgy ? ` (${item.lgy})` : '';
-           const variant = item.variant ? `<br><span class="label2">${item.variant} Edition</span>` : '';
-           table += `<p>${item.title}${issue}${lgy}${variant}</p></div>`;
-           // Assign a unique ID for the collapsible button and content
-           table += `<button class="collapsible" id="aboutMeButton-${index}" data-target-id="aboutMeContent-${index}">More info</button>`;
-         } else if (column === 'qty') {
-           table += `<div id="aboutMeContent-${index}" class="content-1" aria-hidden="true"><p><span class="label">Qty:</span> ${item.qty}</p>`;
-         } else if (column === 'publisher') {
-           const shortname = item.shortname ? ` (${item.shortname})` : '';
-           table += `<p><span class="label">Publisher:</span> ${item.publisher}${shortname}</p>`;
-         } else if (column === 'year') {
-           const month = item.month ? `-${item.month}` : '';
-           const day = item.day ? `-${item.day}` : '';
-           table += `<p><span class="label">Release Year:</span> <time datetime="${item.year}${month}${day}">${item.year}</time></p>`;
-         } else if (column === 'writers') {
-           const writers = Array.isArray(item[column]) ? item[column].join(', ') : item[column];
-           table += `<p><span class="label">Writer(s):</span> ${writers}</p>`;
-         } else if (column === 'illustrators') {
-           const illustrators = Array.isArray(item[column]) ? item[column].join(', ') : item[column];
-           table += `<p><span class="label">Illustrator(s):</span> ${illustrators}</p>`;
-         } else if (column === 'era') {
-           table += `<p><span class="label">Era:</span> ${item.era}</p>`;
-         } else if (column === 'cgc') {
-           table += `<p><span class="label">Approx CGC Grade:</span> ${item.cgc}</p>`;
-         } else if (column === 'idLink') {
-           table += `<p>Database Link: <a target="_blank" tabindex="-1" href="https://www.comics.org/issue/${item.id}/">${item.id}</a></p>`;
-         } else if (column === 'notes') {
-           const hasNotes = Array.isArray(item[column]) && item[column].some(note => note.trim() !== '');
-           const ifnotes = hasNotes ? item[column].join('<br><br>') : ' <span class="label2">none</span>';
-           table += `<p><br><span class="label">Notes:</span>${hasNotes ? '<br>' : ''}${ifnotes}</p></div>`;
-         } else {
-           table += ``;
-         }
-       });
-       table += '</li>';
-     });
-     table += '</ul>';
+  const columnOrder = ["idFile", "title", "qty", "publisher", "year", "writers", "illustrators", "era", "cgc", "idLink", "notes"];
+  
+  fetch('/comics/comics.json')
+    .then(response => response.json())
+    .then(data => {
+      let table = '<ul class="comic-grid">';
+      data.forEach((item, index) => {
+        table += '<li class="comic">';
+        columnOrder.forEach(column => {
+          if (column === 'idFile') {
+            table += `<div><img class="comic-img" alt="Comic cover" src="/comics/img/covers/${item.id}.avif">`;
+          } else if (column === 'title') {
+            const issue = item.issue ? ` #${item.issue}` : '';
+            const lgy = item.lgy ? ` (${item.lgy})` : '';
+            const variant = item.variant ? `<br><span class="label2">${item.variant} Edition</span>` : '';
+            table += `<p class="comic-title">${item.title}${issue}${lgy}${variant}</p></div>`;
+            // Wrap the button and content in a relative container
+            table += `<div class="comic-content-wrapper">`;
+            table += `<button class="collapsible" id="aboutMeButton-${index}" data-target-id="aboutMeContent-${index}">More info</button>`;
+          } else if (column === 'qty') {
+            table += `<div id="aboutMeContent-${index}" class="content-1" aria-hidden="true"><p style="margin-top: 15px"><span class="label">Qty:</span> ${item.qty}</p>`;
+          } else if (column === 'publisher') {
+            const shortname = item.shortname ? ` (${item.shortname})` : '';
+            table += `<p><span class="label">Publisher:</span> ${item.publisher}${shortname}</p>`;
+          } else if (column === 'year') {
+            const month = item.month ? `-${item.month}` : '';
+            const day = item.day ? `-${item.day}` : '';
+            table += `<p><span class="label">Release Year:</span> <time datetime="${item.year}${month}${day}">${item.year}</time></p>`;
+          } else if (column === 'writers') {
+            const writers = Array.isArray(item[column]) ? item[column].join(', ') : item[column];
+            table += `<p><span class="label">Writer(s):</span> ${writers}</p>`;
+          } else if (column === 'illustrators') {
+            const illustrators = Array.isArray(item[column]) ? item[column].join(', ') : item[column];
+            table += `<p><span class="label">Illustrator(s):</span> ${illustrators}</p>`;
+          } else if (column === 'era') {
+            table += `<p><span class="label">Era:</span> ${item.era}</p>`;
+          } else if (column === 'cgc') {
+            table += `<p><span class="label">Approx CGC Grade:</span> ${item.cgc}</p>`;
+          } else if (column === 'idLink') {
+            table += `<p>Database Link: <a target="_blank" tabindex="-1" href="https://www.comics.org/issue/${item.id}/">${item.id}</a></p>`;
+          } else if (column === 'notes') {
+            const hasNotes = Array.isArray(item[column]) && item[column].some(note => note.trim() !== '');
+            const ifnotes = hasNotes ? item[column].join('<br><br>') : ' <span class="label2">none</span>';
+            table += `<p><br><span class="label">Notes:</span>${hasNotes ? '<br>' : ''}${ifnotes}</p></div></div>`; // Close the wrapper div
+          } else {
+            table += ``;
+          }
+        });
+        table += '</li>';
+      });
+      table += '</ul>';
 
-     document.getElementById('table-container').innerHTML = table;
-     // Initialize collapsibles after content is added
-     initializeCollapsible();
-   })
-   .catch(error => console.error('Error loading table:', error));
- console.log('Comic grid initialized');
+      document.getElementById('table-container').innerHTML = table;
+      // Initialize collapsibles after content is added
+      initializeCollapsible();
+    })
+    .catch(error => console.error('Error loading table:', error));
+  console.log('Comic grid initialized');
 };
