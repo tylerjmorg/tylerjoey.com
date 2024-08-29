67ec06ac4c27e305426e4a664ae311b4f080b565
Tyler Morgan <git@tylerjm.org>
2024-08-29 01:02:07 -0600

    Start v2 work

--- comics/comicgrid.js
+++ comics/comicgrid.js
@@ -1,5 +1,5 @@
 function initializeComicGrid() {
-  const columnOrder = ["idFile", "title", "qty", "publisher", "year", "writers", "illustrators", "era", "cgc", "idLink", "notes"];
+  const columnOrder = ["idFile", "title", "qty", "publisher", "year", "writers", "illustrators", "age", "cgc", "approx_cgc", "idLink", "notes"];
   
   fetch('/comics/comics.json')
     .then(response => response.json())
@@ -9,12 +9,19 @@ function initializeComicGrid() {
         table += '<li class="comic">';
         columnOrder.forEach(column => {
           if (column === 'idFile') {
-            table += `<div><img class="comic-img" alt="Comic cover" src="/comics/img/covers/${item.id}.avif">`;
+            const mod = item.mod_id ? `-${item.mod_id}` : '';
+            table += `<div><img class="comic-img" alt="A comic cover of ${item.cover_alt}" src="/comics/img/covers/${item.issue_id}${mod}.avif">`;
           } else if (column === 'title') {
-            const issue = item.issue ? ` #${item.issue}` : '';
+            const issuePrefix = item.issue_prefix ? `${item.issue_prefix}` : '';
+            const issueSuffix = item.issue_suffix ? `${item.issue_suffix}` : '';
+            const issue = item.issue ? `#${item.issue}` : '';
+            const issueDetails = issuePrefix || issue || issueSuffix ? ` ${issuePrefix}${issue}${issueSuffix}` : '';
             const lgy = item.lgy ? ` (${item.lgy})` : '';
-            const variant = item.variant ? `<br><span class="label2">${item.variant} Edition</span>` : '';
-            table += `<p class="comic-title">${item.title}${issue}${lgy}${variant}</p></div>`;
+            const variant = item.variant ? `${item.variant} Edition` : '';
+            const printing = item.printing ? `${item.printing} Printing` : '';
+            const variant_printing = variant && printing ? `${variant}, ${printing}` : `${variant}${printing}`;
+            const variant_printing_html = variant_printing ? `<br><span class="label2">${variant_printing}</span>` : '';
+            table += `<p class="comic-title">${item.title}${issueDetails}${lgy}${variant_printing_html}</p></div>`;
             // Wrap the button and content in a relative container
             table += `<div class="comic-content-wrapper">`;
             table += `<button class="collapsible" id="aboutMeButton-${index}" data-target-id="aboutMeContent-${index}">More info</button>`;
@@ -24,8 +31,8 @@ function initializeComicGrid() {
             const shortname = item.shortname ? ` (${item.shortname})` : '';
             table += `<p><span class="label">Publisher:</span> ${item.publisher}${shortname}</p>`;
           } else if (column === 'year') {
-            const month = item.month ? `-${item.month}` : '';
-            const day = item.day ? `-${item.day}` : '';
+            const month = item.month ? `-${String(item.month).padStart(2, '0')}` : '';
+            const day = item.day ? `-${String(item.day).padStart(2, '0')}` : '';
             table += `<p><span class="label">Release Year:</span> <time datetime="${item.year}${month}${day}">${item.year}</time></p>`;
           } else if (column === 'writers') {
             const writers = Array.isArray(item[column]) ? item[column].join(', ') : item[column];
@@ -33,12 +40,18 @@ function initializeComicGrid() {
           } else if (column === 'illustrators') {
             const illustrators = Array.isArray(item[column]) ? item[column].join(', ') : item[column];
             table += `<p><span class="label">Illustrator(s):</span> ${illustrators}</p>`;
-          } else if (column === 'era') {
-            table += `<p><span class="label">Era:</span> ${item.era}</p>`;
+          } else if (column === 'age') {
+            table += `<p><span class="label">Age:</span> ${item.age}</p>`;
           } else if (column === 'cgc') {
-            table += `<p><span class="label">Approx CGC Grade:</span> ${item.cgc}</p>`;
+            const cgcValue = item.cgc ? (item.cgc === 10 ? '10' : item.cgc.toFixed(1)) : '';
+            const cgc = cgcValue ? `<p><span class="label">CGC Grade:</span> ${cgcValue}</p>` : '';
+            table += cgc;
+          } else if (column === 'approx_cgc') {
+            const approxCgcValue = item.approx_cgc ? (item.approx_cgc === 10 ? '10' : item.approx_cgc.toFixed(1)) : '';
+            const approx_cgc = approxCgcValue ? `<p><span class="label">Approx CGC Grade:</span> ${approxCgcValue}</p>` : '';
+            table += approx_cgc;
           } else if (column === 'idLink') {
-            table += `<p>Database Link: <a target="_blank" tabindex="-1" href="https://www.comics.org/issue/${item.id}/">${item.id}</a></p>`;
+            table += `<p>Database Link: <a target="_blank" tabindex="-1" href="https://www.comics.org/issue/${item.issue_id}/">${item.issue_id}</a></p>`;
           } else if (column === 'notes') {
             const hasNotes = Array.isArray(item[column]) && item[column].some(note => note.trim() !== '');
             const ifnotes = hasNotes ? item[column].join('<br><br>') : ' <span class="label2">none</span>';
